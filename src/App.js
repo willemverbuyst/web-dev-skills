8e43a414210f8b437d60668af38a714e2b447cf0
Willem Verbuyst <willemverbuyst@gmail.com>
2020-06-28 18:01:28 +0200

    Install threejs and stars

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Canvas } from 'react-three-fiber';
+import { OrbitControls, Stars } from 'drei';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <Canvas style={{ height: '100vh' }}>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Stars />
+        <OrbitControls />
+      </Canvas>
+    </>
+  );
 }
 
 export default App;
