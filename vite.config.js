e3d03b64c43e33c67c42f7c73f5fe2d34f63262b
Willem Verbuyst <willemverbuyst@gmail.com>
2025-10-24 05:04:36 +0200

    Add base to config for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig(() => {
   return {
     plugins: [react()],
+    base: "/web-dev-skills/",
     server: {
       port: 3000,
     },
